379289845c58ad431bd013bf99a62102f4efe679
JericoLuzardoMiranda <125827592+JericoLuzardoMiranda@users.noreply.github.com>
2024-11-02 16:11:16 +0000

    Completed a dessert release app

---
+++ Persistencia de datos/DessertRelease/app/src/main/java/com/example/dessertrelease/ui/theme/Color.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.dessertrelease.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val md_theme_light_primary = Color(0xFF4CAF50)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFC8E6C9)
+val md_theme_light_onPrimaryContainer = Color(0xFF1B5E20)
+val md_theme_light_secondary = Color(0xFF81C784)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFA5D6A7)
+val md_theme_light_onSecondaryContainer = Color(0xFF2E7D32)
+val md_theme_light_tertiary = Color(0xFF66BB6A)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFC8E6C9)
+val md_theme_light_onTertiaryContainer = Color(0xFF1B5E20)
+val md_theme_light_error = Color(0xFFD32F2F)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_errorContainer = Color(0xFFFDE0DC)
+val md_theme_light_onErrorContainer = Color(0xFFB71C1C)
+val md_theme_light_outline = Color(0xFFB0BEC5)
+val md_theme_light_background = Color(0xFFF1F8E9)
+val md_theme_light_onBackground = Color(0xFF1B5E20)
+val md_theme_light_surface = Color(0xFFF1F8E9)
+val md_theme_light_onSurface = Color(0xFF1B5E20)
+val md_theme_light_surfaceVariant = Color(0xFFC8E6C9)
+val md_theme_light_onSurfaceVariant = Color(0xFF4CAF50)
+val md_theme_light_inverseSurface = Color(0xFF2E7D32)
+val md_theme_light_inverseOnSurface = Color(0xFFC8E6C9)
+val md_theme_light_inversePrimary = Color(0xFF388E3C)
+val md_theme_light_shadow = Color(0xFF000000)
+val md_theme_light_surfaceTint = Color(0xFF4CAF50)
+val md_theme_light_outlineVariant = Color(0xFFAED581)
+val md_theme_light_scrim = Color(0xFF000000)
+
+
+val md_theme_dark_primary = Color(0xFFA5D6A7)
+val md_theme_dark_onPrimary = Color(0xFF1B5E20)
+val md_theme_dark_primaryContainer = Color(0xFF388E3C)
+val md_theme_dark_onPrimaryContainer = Color(0xFFC8E6C9)
+val md_theme_dark_secondary = Color(0xFF81C784)
+val md_theme_dark_onSecondary = Color(0xFF1B5E20)
+val md_theme_dark_secondaryContainer = Color(0xFF4CAF50)
+val md_theme_dark_onSecondaryContainer = Color(0xFFA5D6A7)
+val md_theme_dark_tertiary = Color(0xFF66BB6A)
+val md_theme_dark_onTertiary = Color(0xFFFFFFFF)
+val md_theme_dark_tertiaryContainer = Color(0xFF388E3C)
+val md_theme_dark_onTertiaryContainer = Color(0xFFC8E6C9)
+val md_theme_dark_error = Color(0xFFEF5350)
+val md_theme_dark_onError = Color(0xFFB71C1C)
+val md_theme_dark_errorContainer = Color(0xFFD32F2F)
+val md_theme_dark_onErrorContainer = Color(0xFFF9D5D4)
+val md_theme_dark_outline = Color(0xFFB0BEC5)
+val md_theme_dark_background = Color(0xFF1B5E20)
+val md_theme_dark_onBackground = Color(0xFFC8E6C9)
+val md_theme_dark_surface = Color(0xFF1B5E20)
+val md_theme_dark_onSurface = Color(0xFFA5D6A7)
+val md_theme_dark_surfaceVariant = Color(0xFF4CAF50)
+val md_theme_dark_onSurfaceVariant = Color(0xFFC8E6C9)
+val md_theme_dark_inverseSurface = Color(0xFFC8E6C9)
+val md_theme_dark_inverseOnSurface = Color(0xFF2E7D32)
+val md_theme_dark_inversePrimary = Color(0xFF4CAF50)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_surfaceTint = Color(0xFF81C784)
+val md_theme_dark_outlineVariant = Color(0xFF66BB6A)
+val md_theme_dark_scrim = Color(0xFF000000)
